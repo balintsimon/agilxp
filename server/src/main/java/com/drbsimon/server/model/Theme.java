5321fd2db5e71faa5993997092dd6938090d6f6a
dr. Simon Bálint <balladin0@gmail.com>
2020-11-14 23:54:28 +0100

    Add basic theme entity model

---
+++ server/src/main/java/com/drbsimon/server/model/Theme.java
@@ -0,0 +1,27 @@
+package com.drbsimon.server.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Theme {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonManagedReference
+    @OneToMany(mappedBy = "member", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Member> members;
+}
---
+++ server/src/main/java/com/drbsimon/server/repository/ThemeRepository.java
@@ -0,0 +1,9 @@
+package com.drbsimon.server.repository;
+
+import com.drbsimon.server.model.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+}
