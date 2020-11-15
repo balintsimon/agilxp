831aebe1cba9adef54949b130ed9c1fce0dab75b
dr. Simon Bálint <balladin0@gmail.com>
2020-11-15 21:15:01 +0100

    Add registering new user service

--- server/src/main/java/com/drbsimon/server/service/UserService.java
+++ server/src/main/java/com/drbsimon/server/service/UserService.java
@@ -6,12 +6,15 @@ import com.drbsimon.server.model.AppUser;
 import com.drbsimon.server.model.Role;
 import com.drbsimon.server.model.UserGroup;
 import com.drbsimon.server.model.dto.NewGroupDto;
+import com.drbsimon.server.model.dto.NewUserDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Data
 @RequiredArgsConstructor
@@ -30,11 +33,35 @@ public class UserService {
         return true;
     }
 
+    public boolean tryRegisterNewUser(NewUserDto newUserDto) {
+        if (isNewUserRegistrationRequestInvalid(newUserDto)
+                || appUserDao.exists(newUserDto.getNewUserName())
+                || !isRequesterGroupAdmin(newUserDto.getRequesterName(), newUserDto.getGroupId())
+        ) return false;
+        saveNewUser(newUserDto);
+        return true;
+    }
+
     private boolean isGroupRegistrationRequestInvalid(NewGroupDto newGroupDto) {
         return newGroupDto.getGroupName() == null
                 || newGroupDto.getUserName() == null
                 || newGroupDto.getGroupName().isEmpty()
                 || newGroupDto.getUserName().isEmpty();
+    }
+
+    private boolean isNewUserRegistrationRequestInvalid(NewUserDto newUserDto) {
+        return newUserDto.getGroupId() == null
+                || newUserDto.getNewUserName() == null
+                || newUserDto.getRequesterName() == null
+                || newUserDto.getNewUserName().isEmpty()
+                || newUserDto.getRequesterName().isEmpty();
+    }
+
+    protected boolean isRequesterGroupAdmin(String requesterName, Long groupId) {
+        AppUser requester = appUserDao.getBy(requesterName);
+        return requester == null
+                && requester.getRole() == Role.ADMIN
+                && requester.getUserGroup().getId().equals(groupId);
     }
 
     private void saveNewGroup(NewGroupDto newGroupDto) {
@@ -51,6 +78,23 @@ public class UserService {
         newGroup.setAppUsers(Arrays.asList(newAdmin));
 
         userGroupDao.save(newGroup);
+    }
+
+    private void saveNewUser(NewUserDto newUserDto) {
+        AppUser newUser = new AppUser().builder()
+                .name(newUserDto.getNewUserName())
+                .role(Role.USER)
+                .build();
+
+        UserGroup existingGroup = userGroupDao.getBy(newUserDto.getGroupId());
+        List<AppUser> existingUsers = existingGroup.getAppUsers();
+
+        newUser.setUserGroup(existingGroup);
+
+        existingUsers.add(newUser);
+        existingGroup.setAppUsers(existingUsers);
+
+        userGroupDao.save(existingGroup);
     }
 
 }
--- server/src/test/java/com/drbsimon/server/service/UserServiceTest.java
+++ server/src/test/java/com/drbsimon/server/service/UserServiceTest.java
@@ -2,7 +2,11 @@ package com.drbsimon.server.service;
 
 import com.drbsimon.server.dao.AppUserDao;
 import com.drbsimon.server.dao.UserGroupDao;
+import com.drbsimon.server.model.AppUser;
+import com.drbsimon.server.model.Role;
+import com.drbsimon.server.model.UserGroup;
 import com.drbsimon.server.model.dto.NewGroupDto;
+import com.drbsimon.server.model.dto.NewUserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,7 +38,7 @@ class UserServiceTest {
     }
 
     @Test
-    public void testRegisterWithNullInRegistrationForm() {
+    public void testRegisterGroupWithNullInRegistrationForm() {
         String userName = null;
         String groupName = "Group";
         boolean expected = false;
@@ -50,7 +54,7 @@ class UserServiceTest {
     }
 
     @Test
-    public void testRegisterWithEmptyStringInRegistrationForm() {
+    public void testRegisterGroupWithEmptyStringInRegistrationForm() {
         String userName = "";
         String groupName = "Group";
         boolean expected = false;
@@ -66,7 +70,7 @@ class UserServiceTest {
     }
 
     @Test
-    public void testRegisterWithUserNameTaken() {
+    public void testRegisterGroupWithUserNameTaken() {
         String userName = "Username";
         String groupName = "Group";
         boolean expected = false;
@@ -84,7 +88,7 @@ class UserServiceTest {
     }
 
     @Test
-    public void testRegisterWithGroupNameTaken() {
+    public void testRegisterGroupWithGroupNameTaken() {
         String userName = "Username";
         String groupName = "Group";
         boolean expected = false;
@@ -102,7 +106,7 @@ class UserServiceTest {
     }
 
     @Test
-    public void testSuccessfulRegister() {
+    public void testSuccessfulNewGroupRegister() {
         String userName = "Username";
         String groupName = "Group";
         boolean expected = true;
@@ -115,6 +119,126 @@ class UserServiceTest {
         given(appUserDao.exists(userName)).willReturn(false);
         given(userGroupDao.exists(groupName)).willReturn(false);
         boolean actual = service.tryRegisterGroup(newGroupDto);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testAddNewUserWithNullInForm() {
+        String newUserName = null;
+        String adminName = "Admin";
+        Long groupId = 1L;
+        boolean expected = false;
+
+        NewUserDto newUserDto = NewUserDto.builder()
+                .newUserName(newUserName)
+                .requesterName(adminName)
+                .groupId(groupId)
+                .build();
+
+        boolean actual = service.tryRegisterNewUser(newUserDto);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testRegisterNewUserWithEmptyStringInForm() {
+        String newUserName = "";
+        String adminName = "Admin";
+        Long groupId = 1L;
+        boolean expected = false;
+
+        NewUserDto newUserDto = NewUserDto.builder()
+                .newUserName(newUserName)
+                .requesterName(adminName)
+                .groupId(groupId)
+                .build();
+
+        boolean actual = service.tryRegisterNewUser(newUserDto);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testAddingNewUserByNotAdmin() {
+        String newUserName = "New User";
+        String adminName = "Admin";
+        Long groupId = 1L;
+        boolean expected = false;
+
+        NewUserDto newUserDto = NewUserDto.builder()
+                .newUserName(newUserName)
+                .requesterName(adminName)
+                .groupId(groupId)
+                .build();
+
+        given(appUserDao.getBy(adminName)).willReturn(AppUser.builder()
+                .name(adminName)
+                .role(Role.USER)
+                .userGroup(UserGroup.builder()
+                        .name("group")
+                        .id(groupId)
+                        .build())
+                .build());
+        given(appUserDao.exists(newUserName)).willReturn(false);
+
+        boolean actual = service.tryRegisterNewUser(newUserDto);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testAddingNewUserByOtherGroupAdmin() {
+        String newUserName = "New User";
+        String adminName = "Admin";
+        Long groupId = 1L;
+        boolean expected = false;
+
+        NewUserDto newUserDto = NewUserDto.builder()
+                .newUserName(newUserName)
+                .requesterName(adminName)
+                .groupId(groupId)
+                .build();
+
+        given(appUserDao.getBy(adminName)).willReturn(AppUser.builder()
+                .name(adminName)
+                .role(Role.ADMIN)
+                .userGroup(UserGroup.builder()
+                        .name("group")
+                        .id(groupId + 1L)
+                        .build())
+                .build());
+        given(appUserDao.exists(newUserName)).willReturn(false);
+
+        boolean actual = service.tryRegisterNewUser(newUserDto);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testNewUserNameTaken() {
+        String newUserName = "New User";
+        String adminName = "Admin";
+        Long groupId = 1L;
+        boolean expected = false;
+
+        NewUserDto newUserDto = NewUserDto.builder()
+                .newUserName(newUserName)
+                .requesterName(adminName)
+                .groupId(groupId)
+                .build();
+
+        given(appUserDao.getBy(adminName)).willReturn(AppUser.builder()
+                .name(adminName)
+                .role(Role.ADMIN)
+                .userGroup(UserGroup.builder()
+                        .name("group")
+                        .id(groupId)
+                        .build())
+                .build());
+        given(appUserDao.exists(newUserName)).willReturn(true);
+
+        boolean actual = service.tryRegisterNewUser(newUserDto);
 
         assertThat(actual).isEqualTo(expected);
     }
