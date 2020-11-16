7b11670b93f31a7a255e5d73dbd6feeb6ebadb6f
dr. Simon Bálint <balladin0@gmail.com>
2020-11-16 11:49:27 +0100

    Add main menu customization options

--- server/src/main/java/com/drbsimon/server/controller/UserController.java
+++ server/src/main/java/com/drbsimon/server/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.drbsimon.server.controller;
 
-import com.drbsimon.server.model.dto.AppUserDto;
-import com.drbsimon.server.model.dto.NewGroupDto;
-import com.drbsimon.server.model.dto.NewUserDto;
+import com.drbsimon.server.model.MainMenu;
+import com.drbsimon.server.model.dto.*;
 import com.drbsimon.server.model.wrapper.AppUserWrapper;
+import com.drbsimon.server.service.BackgroundService;
+import com.drbsimon.server.service.IconService;
+import com.drbsimon.server.service.MainMenuService;
 import com.drbsimon.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class UserController {
     private final UserService userService;
+    private final IconService iconService;
+    private final MainMenuService mainMenuService;
+    private final BackgroundService backgroundService;
 
     @GetMapping
     public AppUserWrapper getAllUsers() {
@@ -27,9 +32,29 @@ public class UserController {
         return userService.tryRegisterNewUser(newUserDto);
     }
 
+    @GetMapping("/{id}")
+    public MainMenu getUserMainMenu(@PathVariable Long id) {
+        return mainMenuService.getMainMenuByUser(id);
+    }
+
     @PostMapping("/group")
     public boolean registerNewGroup(@RequestBody NewGroupDto newGroupDto) {
         return userService.tryRegisterGroup(newGroupDto);
+    }
+
+    @PostMapping("/icon")
+    public boolean addNewIcon(@RequestBody IconDto iconDto) {
+        return iconService.addNewIcon(iconDto);
+    }
+
+    @PostMapping("/add_background")
+    public boolean addNewBackground(@RequestBody BackgroundRequestDto backgroundRequestDto) {
+        return backgroundService.addNewBackground(backgroundRequestDto);
+    }
+
+    @PostMapping("/background")
+    public boolean changeBackground(@RequestBody BackgroundRequestDto backgroundRequestDto) {
+        return backgroundService.setBackgroundToMainMenu(backgroundRequestDto);
     }
 
     @PutMapping
