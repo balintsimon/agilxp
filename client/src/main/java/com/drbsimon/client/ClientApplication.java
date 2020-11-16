88919a38ea608d2628a46ae71e44f27cbac12b7b
dr. Simon Bálint <balladin0@gmail.com>
2020-11-16 11:54:48 +0100

    Add commandLineRunner for running tester app

--- client/src/main/java/com/drbsimon/client/ClientApplication.java
+++ client/src/main/java/com/drbsimon/client/ClientApplication.java
@@ -1,13 +1,20 @@
 package com.drbsimon.client;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientApplication {
+@Slf4j
+public class ClientApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        log.info("EXECUTING test application");
+    }
 }
