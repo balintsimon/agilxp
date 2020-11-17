9e265e48f64815bbbefcea998c97509d67433dbf
dr. Simon Bálint <balladin0@gmail.com>
2020-11-17 15:21:48 +0100

    Add service to handle new group registration

--- client/src/main/java/com/drbsimon/client/service/ServerCallerService.java
+++ client/src/main/java/com/drbsimon/client/service/ServerCallerService.java
@@ -1,6 +1,7 @@
 package com.drbsimon.client.service;
 
 import com.drbsimon.client.caller.ServerCaller;
+import com.drbsimon.client.caller.model.dto.*;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,4 +13,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ServerCallerService {
     private final ServerCaller serverCaller;
+
+    public boolean registerNewGroup(String groupName, String adminName) {
+        NewGroupDto newGroupDto = NewGroupDto.builder()
+                .groupName("New Group")
+                .userName("New Admin")
+                .build();
+        return serverCaller.registerNewGroup(newGroupDto);
+    }
 }
