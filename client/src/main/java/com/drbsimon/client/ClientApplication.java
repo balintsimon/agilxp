a40e9989c9f828150f3a0dbd6d76b8d9200e7e0d
dr. Simon Bálint <balladin0@gmail.com>
2020-11-17 15:16:50 +0100

    Add load balanced RestTemplate

--- client/src/main/java/com/drbsimon/client/ClientApplication.java
+++ client/src/main/java/com/drbsimon/client/ClientApplication.java
@@ -4,10 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Slf4j
 public class ClientApplication implements CommandLineRunner {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
